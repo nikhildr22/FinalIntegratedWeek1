fbf842b05b27030099796fbb7f0dbc8a376cc7c7
nikhildr22 <nikhildr22@gmail.com>
2021-05-04 16:42:55 +0530

    added vegetable class

--- FinalIntegratedWeek1/Program.cs
+++ FinalIntegratedWeek1/Program.cs
@@ -2,6 +2,20 @@
 
 namespace FinalIntegratedWeek1
 {
+    class Vegitable
+    {
+        private string _name;
+        private double _price;
+
+        public Vegitable(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public double Price { get => _price; set => _price = value; }
+    }
     class Program
     {
         static void Main(string[] args)
